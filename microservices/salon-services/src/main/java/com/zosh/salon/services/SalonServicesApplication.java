9fad6a7ce20378eed31b713b3a1e953a1baa48ec
utkarshatre <utkarsh3336@gmail.com>
2025-05-20 22:13:22 +0530

    microservices

---
+++ microservices/salon-services/src/main/java/com/zosh/salon/services/SalonServicesApplication.java
@@ -0,0 +1,13 @@
+package com.zosh.salon.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonServicesApplication.class, args);
+	}
+
+}
---
+++ microservices/salon-services/src/test/java/com/zosh/salon/services/SalonServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zosh.salon.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
