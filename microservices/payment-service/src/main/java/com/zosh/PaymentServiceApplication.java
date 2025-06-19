b0968dbdbacd7c7475edf154066f8eb5dab5f8aa
utkarshatre <utkarsh3336@gmail.com>
2025-06-19 22:44:49 +0530

    new payment_service

---
+++ microservices/payment-service/src/main/java/com/zosh/PaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceApplication.class, args);
+		System.out.println("++++++++++++++++++++++++(Working Fine)++++++++++++++++++++++++++++");
+	}
+}
